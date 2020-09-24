664f135515eb07f27d4590b2ab97dc3e5e2182c2
Hans Schouten <hans@falcosolutions.nl>
2020-09-24 08:53:17 +0200

    Improve homepage

--- public/index.php
+++ public/index.php
@@ -17,72 +17,111 @@ function getAdviceValue($countryData): int
     }
     return 0;
 }
-?>
+function getAdviceText($countryData): string
+{
+    $codes = [];
+    if ($countryData['code_red']) {
+        $codes[] = 'Rood';
+    } elseif ($countryData['code_orange']) {
+        $codes[] = 'Oranje';
+    } elseif ($countryData['code_yellow']) {
+        $codes[] = 'Geel';
+    } elseif ($countryData['code_green']) {
+        $codes[] = 'Groen';
+    }
 
+    return 'Code: ' . implode(',', $codes) . "<br><small>Klik voor meer informatie</small>";
+}
+?>
 <!DOCTYPE html>
+<html lang="nl">
 <head>
+    <meta charset="utf-8">
+    <title>Doe voor vertrek.. de corona reis check!</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Doe voor vertrek, de corona reis check">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,700">
-    <title>Corona Reis Check</title>
+
+    <style>
+        h1 {
+            font-family: "Poppins", sans-serif;
+            font-weight: 200;
+            margin-bottom: 45px;
+            font-size: 30px;
+            text-align: center;
+        }
+
+        #visualization {
+            width: 1200px;
+            margin: 0 auto;
+        }
+
+        .bottom-left {
+            position: fixed;
+            bottom: 5px;
+            left: 10px;
+        }
+        .bottom-left img {
+            width: 200px;
+        }
+    </style>
 </head>
 <body>
+    <h1>Doe voor vertrek.. de corona reis check!</h1>
+    <div id='visualization'></div>
 
-<script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
-<script type='text/javascript'>google.load('visualization', '1', {'packages': ['geochart']});
-    google.setOnLoadCallback(drawVisualization);
+    <a class="bottom-left" href="https://falcotravel.com">
+        <img src="/assets/media/falco-logo.png"></a>
+    </a>
 
-    function drawVisualization() {
-        var data = new google.visualization.DataTable();
+    <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
+    <script type='text/javascript'>google.load('visualization', '1.1', {'packages': ['geochart']});
+        google.setOnLoadCallback(drawVisualization);
 
-        data.addColumn('string', 'Country');
-        data.addColumn('number', 'Value');
-        data.addColumn({type: 'string', role: 'tooltip'});
+        function drawVisualization() {
+            var data = new google.visualization.DataTable();
 
-        <?php
-        foreach ($advicePerCountry as $countryCode => $countryData):
-        ?>
-        data.addRows([[{v: '<?= $countryCode ?>', f: '<?= e($countryData['name']) ?>'}, <?= getAdviceValue($countryData) ?>, '']]);
-        <?php
-        endforeach;
-        ?>
+            data.addColumn('string', 'Country');
+            data.addColumn('number', 'Value');
+            data.addColumn({type: 'string', role: 'tooltip'});
 
-        var options = {
-            backgroundColor: {fill: '#FFFFFF', stroke: '#FFFFFF', strokeWidth: 0},
-            colorAxis: {
-                minValue: 0,
-                maxValue: 4,
-                colors: ['#F5F0E7', '#7FEB00', '#FFFF00', '#FFA000', '#FF0000']
-            },
-            legend: 'none',
-            datalessRegionColor: '#F5F0E7',
-            displayMode: 'regions',
-            enableRegionInteractivity: 'true',
-            resolution: 'countries',
-            sizeAxis: {minValue: 1, maxValue: 1, minSize: 10, maxSize: 10},
-            region: 'world',
-            keepAspectRatio: true,
-            width: 1200,
-            height: 740,
-            tooltip: {textStyle: {color: '#444444'}, trigger: 'focus'}
-        };
+            var data = google.visualization.arrayToDataTable([
+                ['Country', 'Value', {role: 'tooltip', p:{html:true}}],
+                <?php
+                foreach ($advicePerCountry as $countryCode => $countryData):
+                ?>
+                [{v: '<?= $countryCode ?>', f: '<?= e($countryData['name']) ?>'}, <?= getAdviceValue($countryData) ?>, '<?= getAdviceText($countryData)?>'],
+                <?php
+                endforeach;
+                ?>
+            ]);
 
-        var chart = new google.visualization.GeoChart(document.getElementById('visualization'));
-        chart.draw(data, options);
-    }
-</script>
-<h1 style="text-align: center;">Corona Reis Check</h1>
-<div id='visualization'></div>
-<style>
-    h1 {
-        font-family: "Poppins", sans-serif;
-        font-weight: 200;
-        margin-bottom: 45px;
-        font-size: 30px;
-    }
+            var options = {
+                backgroundColor: {fill: '#FFFFFF', stroke: '#FFFFFF', strokeWidth: 0},
+                colorAxis: {
+                    minValue: 0,
+                    maxValue: 4,
+                    colors: ['#F5F0E7', '#7FEB00', '#FFFF00', '#FFA000', '#FF0000']
+                },
+                legend: 'none',
+                datalessRegionColor: '#F5F0E7',
+                displayMode: 'regions',
+                enableRegionInteractivity: 'true',
+                resolution: 'countries',
+                sizeAxis: {minValue: 1, maxValue: 1, minSize: 10, maxSize: 10},
+                region: '150',
+                keepAspectRatio: false,
+                width: 1200,
+                height: 740,
+                magnifyingGlass: {enable: true, zoomFactor: 7.5},
+                tooltip: {textStyle: {color: '#444444'}, trigger: 'focus', isHtml: true}
+            };
 
-    #visualization {
-        width: 1200px;
-        margin: 0 auto;
-    }
-</style>
+            var chart = new google.visualization.GeoChart(document.getElementById('visualization'));
+            chart.draw(data, options);
+        }
+    </script>
 </body>
 </html>
