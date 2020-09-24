cf245579fd757648329afb1153899479ac6330d7
csch89 <keesschouten@outlook.com>
2020-09-24 16:50:31 +0200

    Added google Tag Manager

--- public/index.php
+++ public/index.php
@@ -60,6 +60,14 @@ $lastUpdatedAt = Carbon::parse(filemtime(__DIR__ . '/../data/downloads/latest.js
             width: 200px;
         }
     </style>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MVCNMFR');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
     <div class="container">
@@ -142,5 +150,13 @@ $lastUpdatedAt = Carbon::parse(filemtime(__DIR__ . '/../data/downloads/latest.js
             }, 200);
         });
     </script>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVCNMFR"
+                height="0" width="0" style="display:none;visibility:hidden">
+
+        </iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
 </body>
 </html>
