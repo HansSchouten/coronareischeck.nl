fdf00c55a4ec42c3b800b39d262533f9dbf47a96
Hans Schouten <hans@falcosolutions.nl>
2020-09-29 20:02:03 +0200

    Hide Falco logo until fully loaded

--- public/embed/index.php
+++ public/embed/index.php
@@ -99,16 +99,16 @@ $lastUpdatedAt = Carbon::parse(filemtime(__DIR__ . '/../../data/downloads/latest
 <div class="container">
     <div id="map-canvas" class="d-block"></div>
 
-    <a class="bottom-right" href="https://falcotravel.com" target="_blank">
-        <img src="/assets/media/falco-logo.png"></a>
-    </a>
-
     <div class="region-buttons d-none">
         <button data-region="150" class="btn btn-sm btn-primary">Europa</button>
         <button data-region="world" class="btn btn-sm btn-primary">Wereldwijd</button>
     </div>
 
-    <div class="hidden d-none"></div>
+    <div class="hidden d-none">
+        <a class="bottom-right" href="https://falcotravel.com" target="_blank">
+            <img src="/assets/media/falco-logo.png"></a>
+        </a>
+    </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
