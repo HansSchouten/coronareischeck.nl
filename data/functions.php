02b16859ecf6dadf647f4c0044d1851b3ba5c5ff
Hans Schouten <hans@falcosolutions.nl>
2021-06-30 09:08:48 +0200

    Tweak

--- data/functions.php
+++ data/functions.php
@@ -24,7 +24,7 @@ function extractTravelAdvice(&$countryData, $adviceImageUrl)
     $countryData['advice_image_url'] = $adviceImageUrl;
     $adviceImagePalette = Palette::fromFilename($adviceImageUrl);
     foreach ($adviceImagePalette as $intColor => $count) {
-        if ($count < 400) break;
+        if ($count < 300) break;
 
         switch (Color::fromIntToHex($intColor)) {
             case '#FF0000':
